06adeb120973746e3e6e44ed1f6ad06d21c8a23a
Alexander Little <alexanderlittle@Alexanders-MacBook-Pro.local>
2024-05-20 20:06:04 +1200

    first commit

---
+++ form-to-email.php
@@ -0,0 +1,72 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+
+<!-- compose email message  -->
+
+<?php
+	$email_from = 'dd40a3728dc7221bb0e3663faa106e9d';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+
+  
+
+// <!-- send the email  -->
+
+
+
+  $to = "dd40a3728dc7221bb0e3663faa106e9d";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+
+
+
+// <!-- securing from global injection  -->
+
+
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
+
+<?php
+/* Mailhandler for the main email address */
+header ("Location: mailto:dd40a3728dc7221bb0e3663faa106e9d");
+exit();
+?>
